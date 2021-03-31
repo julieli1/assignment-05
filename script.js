25a4d0ddc2f2b08c72b137212569fa878ac7e3de
julieli1 <missvckise@gmail.com>
2021-03-31 13:25:25 +0000

    mostly everything

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ document.getElementById('calculate').addEventListener('click', math)
 let diameter = 0
 let area = 0
 let circumference = 0
-let alphabet = false
-let negative = false
 const pi = 3.14
 
 function math () {
@@ -13,22 +11,21 @@ function math () {
   area = 0
   circumference = 0
   alphabet = false
-  negative = false
 
   // this tells you not to use negative numbers
   if (diameter < 0) {
     document.getElementById('alert').innerHTML = 'please use positive numbers'
   }
 
-  // tells you not to use letters (i'm especially proud of this)
-  if (diameter = /[a-zA-Z]/) {
-    alphabet = !false
-  }
-  if (alphabet = !false) {
+  // tells you not to use letters
+  if (isNaN(diameter)) {
     document.getElementById('alert').innerHTML = 'please enter a numerical value'
   }
 
-  area = ((diameter / 2) ^ 2) * pi
-  document.getElementById('answer').innerHTML = 'area: ' + area + ' cm'
-
+  while (diameter > 0) {
+  area = ((diameter / 2) * (diameter / 2)) * pi
+  circumference = pi * diameter
+  document.getElementById('answera').innerHTML = 'area: ' + area
+  document.getElementById('answerc').innerHTML = 'circumference: ' + circumference 
+  }
 }
